44f744eb377786025715d2b16e7e8331a42847a2
hjkim2040 <hjkim2661@gmail.com>
2023-04-27 13:40:29 +0900

    savedRequet import

--- src/main/java/com/ll/gramgram/base/rq/Rq.java
+++ src/main/java/com/ll/gramgram/base/rq/Rq.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
