b00bda4ca2b23743a27ad9bb340d6d55e825b0b9
hjkim2040 <hjkim2661@gmail.com>
2023-05-11 11:17:12 +0900

    controller -> service

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -134,49 +134,8 @@ public class LikeablePersonController {
         // 인스타인증을 했는지 체크
         if (instaMember != null) {
             // 해당 인스타회원이 좋아하는 사람들 목록
-            Stream<LikeablePerson> likeablePeopleStream = instaMember.getToLikeablePeople().stream();
+            List<LikeablePerson> likeablePeople = likeablePersonService.findByToInstaMember(instaMember, gender, attractiveTypeCode, sortCode);
 
-            if (!gender.isEmpty()) {
-                likeablePeopleStream = likeablePeopleStream
-                        .filter(likeablePerson -> likeablePerson.getFromInstaMember().getGender().equals(gender));
-            }
-
-            if (attractiveTypeCode != 0) {
-                likeablePeopleStream = likeablePeopleStream
-                        .filter(likeablePerson -> likeablePerson.getAttractiveTypeCode() == attractiveTypeCode);
-            }
-
-            switch (sortCode) {
-
-                case 2:
-                    likeablePeopleStream = likeablePeopleStream.sorted(
-                            Comparator.comparing(LikeablePerson::getId)
-                    );
-                    break;
-                case 3:
-                    likeablePeopleStream = likeablePeopleStream.sorted(
-                            Comparator.comparing((LikeablePerson lp) -> lp.getFromInstaMember().getLikes()).reversed()
-                    );
-                    break;
-                case 4:
-                    likeablePeopleStream = likeablePeopleStream.sorted(
-                            Comparator.comparing(lp -> lp.getFromInstaMember().getLikes())
-                    );
-                    break;
-                case 5:
-                    likeablePeopleStream = likeablePeopleStream
-                            .sorted(Comparator.comparing((LikeablePerson lp) -> lp.getFromInstaMember().getGender()).reversed()
-                                    .thenComparing(Comparator.comparing(LikeablePerson::getId).reversed()));
-                    break;
-                case 6:
-                    likeablePeopleStream = likeablePeopleStream
-                            .sorted(Comparator.comparing(LikeablePerson::getAttractiveTypeCode)
-                                    .thenComparing(Comparator.comparing(LikeablePerson::getId).reversed()));
-                    break;
-
-            }
-
-            List<LikeablePerson> likeablePeople = likeablePeopleStream.collect(Collectors.toList());
             model.addAttribute("likeablePeople", likeablePeople);
         }
 
